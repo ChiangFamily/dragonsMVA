06adc43531d126d94d9f418f1a2b4917362c242f
ChiangFamily <todawei@gmail.com>
2017-02-24 14:27:48 +0800

    updated

--- db_create.php
+++ db_create.php
@@ -1,7 +1,7 @@
 <?php
 
  try{
-     $conn = new PDO ( "sqlsrv:server = tcp:dragonsp-test.database.windows.net,1433; Database = dragonsTEST", "david", "Pass54321!!");
+     $conn = new PDO ( "sqlsrv:server = tcp:dragons-test.database.windows.net,1433; Database = dragonsTEST", "david", "Pass54321!!");
      $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
      $sql = "CREATE TABLE dragons (id int IDENTITY(1,1) PRIMARY KEY, name varchar(45) DEFAULT NULL, color varchar(45) DEFAULT NULL, element varchar(45) DEFAULT NULL, moves varchar(45) DEFAULT NULL, created_at datetime DEFAULT NULL, updated_at datetime DEFAULT NULL)";
      $conn->query($sql);
